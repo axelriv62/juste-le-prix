ed76298c06d95e99080d359208a4e22f25b869c3
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2024-10-14 13:51:35 +0200

    Ajout d'une fonction faire des requetes à l'API

--- APIRequest.py
+++ APIRequest.py
@@ -1,3 +1,11 @@
 import requests
+
 url ="http://ws.chez-wam.info/"
 
+def get_data(code):
+    response = requests.get(url + code).json()
+    print(response['images'][0])
+    print(response['price'])
+    print(response['title'])
+
+get_data("B09V356ZBZ")
