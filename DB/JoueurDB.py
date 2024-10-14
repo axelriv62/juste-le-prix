402c70acd954154fa815278e5bbd0f003b4dab1c
Bourhim Bylel <bylel_bourhim@ens.univ-artois.fr>
2024-10-14 13:52:52 +0200

    MàJ Database features Database player fonctionnelle

--- DB/JoueurDB.py
+++ DB/JoueurDB.py
@@ -5,6 +5,13 @@ import sqlite3
 conn = sqlite3.connect('ma_db.db')
 c = conn.cursor()
 
+# Suppression de la table Player si elle existe !TEST UNIQUEMENT!
+# Drop is exist
+# c.execute('''
+#   DROP TABLE IF EXISTS Player
+# ''')
+
+
 # Create table Player
 c.execute('''
     CREATE TABLE Player (
@@ -14,6 +21,13 @@ c.execute('''
         FOREIGN KEY(product_code) REFERENCES Product(product_code)
     )
 ''')
+
+# Insertion de données !TEST UNIQUEMENT!
+# c.execute("INSERT INTO Player VALUES ('Joueur1', 100, 'PADS1')")
+# c.execute("INSERT INTO Player VALUES ('Joueur2', 200, 'P2HSG')")
+# c.execute("INSERT INTO Player VALUES ('Joueur3', 300, 'PCDF3')")
+# c.execute("SELECT * FROM Player")
+# print(c.fetchall())
 
 # Commit les changements et fermer la connexion
 conn.commit()
