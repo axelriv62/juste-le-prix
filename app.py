d4ae1e948147c01b88b1a85c2f30aa80f4f9eb3c
Bourhim Bylel <bylel_bourhim@ens.univ-artois.fr>
2024-11-14 11:42:47 +0100

    ajout pseudo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import APIRequest
+import sqlite3
 
 app = Flask(__name__)
 
@@ -9,11 +10,30 @@ def index():
 
 @app.route('/jeu', methods=['GET'])
 def jeu():
-    code = "B07YQFZ6CJ"
-    image = APIRequest.get_image(code)
-    nom = APIRequest.get_nom(code)
-    prix = APIRequest.get_prix(code)
-    return render_template('jeu.html', image=image, nom=nom, prix=prix)
+    pseudo = request.args.get('pseudo')
+    if not pseudo:
+        return redirect(url_for('index'))
+
+    conn = sqlite3.connect('DB/ma_db.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM Player WHERE player_name = ?", (pseudo,))
+    player_data = c.fetchone()
+    conn.close()
+
+    if player_data:
+        score = player_data[1]
+        product_code = player_data[2]
+        image = APIRequest.get_image(product_code)
+        nom = APIRequest.get_nom(product_code)
+        prix = APIRequest.get_prix(product_code)
+        return render_template('jeu.html', pseudo=pseudo, score=score, image=image, nom=nom, prix=prix)
+    else:
+        # Démarrer un nouveau joueur
+        code = "B07YQFZ6CJ"
+        image = APIRequest.get_image(code)
+        nom = APIRequest.get_nom(code)
+        prix = APIRequest.get_prix(code)
+        return render_template('jeu.html', pseudo=pseudo, image=image, nom=nom, prix=prix)
 
 if __name__ == '__main__':
     app.run(debug=True)
