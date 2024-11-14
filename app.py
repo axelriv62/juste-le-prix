2b4e8add22e7fc7ff4b2530662554ee7c391b057
axelriv62 <axel_riviere@ens.univ-artois.fr>
2024-11-14 11:31:13 +0100

    Séléction aléatoire du produit à deviner

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import APIRequest
+from APIRequest import get_random_product
 
 app = Flask(__name__)
 
@@ -9,11 +10,20 @@ def index():
 
 @app.route('/jeu', methods=['GET'])
 def jeu():
-    code = "B07YQFZ6CJ"
+    code = get_random_product()
     image = APIRequest.get_image(code)
     nom = APIRequest.get_nom(code)
     prix = APIRequest.get_prix(code)
     return render_template('jeu.html', image=image, nom=nom, prix=prix)
+
+@app.route('/jeu', methods=['POST'])
+def jeu_post():
+    nom = request.form['nom']
+    score = request.form['score']
+    code_produit = request.form['code_produit']
+    APIRequest.insert_log(nom, score, code_produit)
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
