e8f4d427e2755c7f31d8c924e5caad989da40447
axelriv62 <axel_riviere@ens.univ-artois.fr>
2024-11-17 17:18:45 +0100

    Correction du score qui ne s'incrémentait plus

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for, session
 from DB import ProduitDB, PartieDB
 
 app = Flask(__name__)
+app.secret_key = 'ekip_abcq'
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -18,6 +19,9 @@ def jeu_get():
     # Récupérer le pseudo et le thème en paramètres
     pseudo = request.args.get('pseudo')
     theme = request.args.get('theme', '')
+
+    # Initialiser le score du joueur
+    session['score'] = 0
 
     # Récupérer les données du produit
     code_produit, nom_produit, image, prix = ProduitDB.get_produit(theme)
@@ -40,27 +44,24 @@ def jeu_post():
     # Récupérer le prix du produit
     actual_prix = ProduitDB.get_prix(code_produit)
 
-    # Initialiser le score
-    score = 0
-
     # Comparer le prix du produit avec le prix proposé par le joueur
     if guess < actual_prix:
         result = "C'est plus !"
-        score += 1
+        session['score'] += 1
         correct_guess = False
     elif guess > actual_prix:
         result = "C'est moins !"
-        score += 1
+        session['score'] += 1
         correct_guess = False
     else:
         result = "Félicitations ! Vous avez trouvé le juste prix ! Essayez de deviner celui du produit suivant..."
-        score += 1
+        session['score'] += 1
         correct_guess = True
 
         # Insérer la partie du joueur dans la base de données
-        PartieDB.inserer_partie(pseudo, score, code_produit)
+        PartieDB.inserer_partie(pseudo, session['score'], code_produit)
 
-    return render_template('jeu.html', pseudo=pseudo, score=score, theme=theme, image=image, nom=nom_produit, code=code_produit, result=result, correct_guess=correct_guess)
+    return render_template('jeu.html', pseudo=pseudo, score=session['score'], theme=theme, image=image, nom=nom_produit, code=code_produit, result=result, correct_guess=correct_guess)
 
 if __name__ == '__main__':
     app.run(debug=True)
