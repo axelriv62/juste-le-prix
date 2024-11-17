d58b8a2f5684e02f64346a4612f403a9bb3cd4c3
axelriv62 <axel_riviere@ens.univ-artois.fr>
2024-11-17 16:26:46 +0100

    Ajout du style de l'affichage du score

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ from DB import ProduitDB, PartieDB
 
 app = Flask(__name__)
 
-# Clé secrète de session (ça permet de stocker les informations de la session en cours, notamment le score du joueur)
-app.secret_key = 'ekip_abcq'
-
-
-
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
@@ -27,8 +22,6 @@ def jeu_get():
     # Récupérer les données du produit
     code_produit, nom_produit, image, prix = ProduitDB.get_produit(theme)
 
-    session['nb_essais'] = 0
-
     return render_template('jeu.html', pseudo=pseudo, theme=theme, score=0, image=image, nom=nom_produit, prix=prix, code=code_produit)
 
 
@@ -47,30 +40,27 @@ def jeu_post():
     # Récupérer le prix du produit
     actual_prix = ProduitDB.get_prix(code_produit)
 
-    # Récupérer le nombre d'essais restants
-    nb_essais = session.get('nb_essais', 0)
+    # Initialiser le score
+    score = 0
 
     # Comparer le prix du produit avec le prix proposé par le joueur
     if guess < actual_prix:
         result = "C'est plus !"
-        nb_essais += 1
+        score += 1
         correct_guess = False
     elif guess > actual_prix:
         result = "C'est moins !"
-        nb_essais += 1
+        score += 1
         correct_guess = False
     else:
-        result = "Félicitations ! Vous avez trouvé le juste prix ! Essayez de deviner le suivant..."
-        nb_essais += 1
+        result = "Félicitations ! Vous avez trouvé le juste prix ! Essayez de deviner celui du produit suivant..."
+        score += 1
         correct_guess = True
 
         # Insérer la partie du joueur dans la base de données
-        PartieDB.inserer_partie(pseudo, nb_essais, code_produit)
+        PartieDB.inserer_partie(pseudo, score, code_produit)
 
-    # Mettre à jour le nombre d'essais restants
-    session['nb_essais'] = nb_essais
-
-    return render_template('jeu.html', pseudo=pseudo, score=nb_essais, theme=theme, image=image, nom=nom_produit, code=code_produit, result=result, correct_guess=correct_guess)
+    return render_template('jeu.html', pseudo=pseudo, score=score, theme=theme, image=image, nom=nom_produit, code=code_produit, result=result, correct_guess=correct_guess)
 
 if __name__ == '__main__':
     app.run(debug=True)
